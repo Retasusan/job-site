ed469b398c12397056ce854a71b541ab16de66d9
Kenta Goto <kenta9736@gmail.com>
2024-09-08 01:04:09 +0900

    ファイルパスの変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./Home";
-import Post from "./Post";
+import Post from "./post";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
